76615ee187da020f83b6bb190028389d152a1232
Jon Vallet <j.vallet@gmail.com>
2025-03-23 09:05:36 +0000

    feat(): Be able to remove items from the cart (#3)
    Also add support to calculate total cost of the shpopping cart

--- src/main/kotlin/com/jonvallet/shopcart/ShoppingCart.kt
+++ src/main/kotlin/com/jonvallet/shopcart/ShoppingCart.kt
@@ -3,14 +3,26 @@ package com.jonvallet.shopcart
 class ShoppingCart {
     private val items = mutableMapOf<Item, Int>()
 
+    fun getShoppingCart(): Map<Item, Int> {
+        return items.toMap()
+    }
+
     fun add(item: Item): ShoppingCart {
         val previousValue = items.getOrDefault(item, 0)
         items[item] = previousValue + 1
         return this
     }
 
-    fun getShoppingCart(): Map<Item, Int> {
-        return items.toMap()
+    fun remove(item: Item): ShoppingCart {
+        val previousValue = items.getOrDefault(item, 0)
+        if(previousValue > 0) {
+            items[item] = previousValue - 1
+        }
+        return this
+    }
+
+    fun getTotal(): Int {
+        return items.map { it.key.price * it.value }.sum()
     }
 }
 
--- src/test/kotlin/com/jonvallet/shopcart/ShoppingCartTest.kt
+++ src/test/kotlin/com/jonvallet/shopcart/ShoppingCartTest.kt
@@ -15,4 +15,38 @@ class ShoppingCartTest {
 		assertEquals(expectedShoppingCart, actualShoppingCart)
 	}
 
+	@Test
+	fun shouldBeAbleToAddSameItemTwice() {
+		val bread = Item("bread", 100)
+		val expectedShoppingCart = mapOf(bread to 2)
+		val actualShoppingCart = ShoppingCart().add(bread).add(bread).getShoppingCart()
+
+		assertEquals(expectedShoppingCart, actualShoppingCart)
+	}
+
+	@Test
+	fun shouldBeAbleToRemoveAnItem() {
+		val bread = Item("bread", 100)
+		val expectedShoppingCart = mapOf(bread to 0)
+		val actualShoppingCart = ShoppingCart().add(bread).remove(bread).getShoppingCart()
+
+		assertEquals(expectedShoppingCart, actualShoppingCart)
+	}
+
+	@Test
+	fun shouldBeAbleToCalculateTotalCost() {
+		val bread = Item("bread", 100)
+		val expectedValue = 200
+		val shoppingCart = ShoppingCart().add(bread).add(bread)
+		assertEquals(expectedValue, shoppingCart.getTotal())
+	}
+
+	@Test
+	fun shouldBeAbleToCalculateTotalCostForMultipleItems() {
+		val bread = Item("bread", 100)
+		val eggs = Item("eggs", 10000)
+		val expectedValue = 10100
+		val shoppingCart = ShoppingCart().add(bread).add(eggs)
+		assertEquals(expectedValue, shoppingCart.getTotal())
+	}
 }
